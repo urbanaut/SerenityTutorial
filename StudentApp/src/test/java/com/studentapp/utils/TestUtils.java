e6ad81a3d3a55e793d6b5d134ddae1df34ab5553
bwitt <bwitt@utah.gov>
2017-09-08 16:02:09 -0600

    Commiting the StudentApp project

---
+++ StudentApp/src/test/java/com/studentapp/utils/TestUtils.java
@@ -0,0 +1,13 @@
+package com.studentapp.utils;
+
+import java.util.Random;
+
+public class TestUtils {
+	
+	public static String getRandomValue() {
+		Random random = new Random();
+		int randomInt = random.nextInt(10000);
+		return String.valueOf(randomInt);
+	}
+
+}
